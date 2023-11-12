0e049c57bb4f32d882ca31ba024ff62d6a9bb86a
Raj Kumar Shrestha <kumarraj.sws@gmail.com>
2023-11-12 13:52:34 +0545

    updated search

--- app/components/Search.tsx
+++ app/components/Search.tsx
@@ -1,18 +1,21 @@
 "use client";
 import { useRouter } from "next/navigation";
+import Loader from "./icons/Loader";
+import SearchIcon from "./icons/SearchIcon";
 
 const Search = () => {
   const router = useRouter();
   let url: string | null = "";
+  let loading: boolean = false;
 
   const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    loading = true;
     if (url) {
       router.push(`/extract?url=${url}`, {
         scroll: true,
       });
-    } else {
-      console.log("url reuired!");
+      loading = false;
     }
   };
 
@@ -40,16 +43,11 @@ const Search = () => {
             type="submit"
             className="group w-[46px] h-[46px] inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg bg-[#b4ff5c] text-neutral-800 disabled:opacity-50 disabled:pointer-events-none"
           >
-            <svg
-              className="group-hover:-rotate-12 duration-300 transition-all"
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              viewBox="0 0 16 16"
-            >
-              <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
-            </svg>
+            {loading ? (
+              <Loader classNames="w-4 h-4 animate-spin" />
+            ) : (
+              <SearchIcon classNames="group-hover:-rotate-12 duration-300 transition-all" />
+            )}
           </button>
         </div>
       </div>
