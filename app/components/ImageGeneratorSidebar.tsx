040f42764729ccc0a23082266b57106885450957
Raj Kumar Shrestha <kumarraj.sws@gmail.com>
2023-11-24 00:25:31 +0545

    added images input

--- app/components/ImageGeneratorSidebar.tsx
+++ app/components/ImageGeneratorSidebar.tsx
@@ -2,7 +2,6 @@
 
 import { useRef, useState } from "react";
 import ArrowUp from "./icons/ArrowUp";
-import { imageToBase64 } from "../utils/convertImage";
 
 type GeneratorSidebarProps = {
   handleTextChange: (title: string, desc: string, url: string) => void;
@@ -14,9 +13,10 @@ const ImageGeneratorSidebar = ({ handleTextChange }: GeneratorSidebarProps) => {
     "Extract meta tags from any URL"
   );
   const [url, setUrl] = useState<string>("www.example.com");
-  const [base64Img, setBase64Img] = useState<string | null | unknown>(null);
-  const fileInput = useRef<HTMLInputElement | null>(null);
+  const logoInput = useRef<HTMLInputElement | null>(null);
+  const mainImageInput = useRef<HTMLInputElement | null>(null);
   const imageHolder = useRef<HTMLDivElement | null>(null);
+  const logoHolder = useRef<HTMLDivElement | null>(null);
 
   const handleTitleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
     setTitle(event.target.value);
@@ -32,51 +32,84 @@ const ImageGeneratorSidebar = ({ handleTextChange }: GeneratorSidebarProps) => {
     setUrl(event.target.value);
   };
 
-  const handleFileInput = async (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
+  const handleFileInput = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.files?.length && imageHolder.current) {
       const uploadedImage = URL.createObjectURL(event.target.files[0]);
       imageHolder.current.style.backgroundImage = `url(${uploadedImage})`;
-      try {
-        // Convert the image file to a Base64 string
-        const base64String: string | null | unknown = await imageToBase64(
-          event.target.files[0]
-        );
-        setBase64Img(base64String);
-      } catch (error) {
-        console.error("Error converting image to Base64:", error);
-      }
+    }
+  };
+
+  const handleLogoInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files?.length && logoHolder.current) {
+      const uploadedImage = URL.createObjectURL(event.target.files[0]);
+      logoHolder.current.style.backgroundImage = `url(${uploadedImage})`;
     }
   };
 
   const triggerFileUpload = () => {
-    fileInput?.current?.click();
+    mainImageInput?.current?.click();
+  };
+
+  const triggerLogoUpload = () => {
+    logoInput?.current?.click();
   };
 
   return (
     <div className="w-full bg-neutral-800 p-4 rounded-xl flex flex-col gap-5 md:sticky md:top-8">
-      <div
-        ref={imageHolder}
-        className="w-full relative py-28 bg-cover bg-center bg-no-repeat rounded-xl overflow-hidden bg-cover bg-no-repeat bg-center bg-blend-overlay"
-      >
-        <div className="absolute inset-0 w-full h-full bg-neutral-600/30 grid place-content-center place-items-center gap-2">
-          <button
-            onClick={triggerFileUpload}
-            className="w-12 h-12 group hover:bg-neutral-100/60 rounded-full bg-neutral-500/50 inline-flex items-center justify-center transition-all duration-300"
-          >
-            <ArrowUp classNames="fill-[#b4ff5c] w-7 transition-all duration-300 group-hover:-translate-y-1 group-hover:fill-neutral-700" />
-          </button>
-          <span className="lg:text-base text-sm text-neutral-200">
-            Add image
-          </span>
-          <input
-            type="file"
-            accept=".jpg, .jpeg, .png, .svg"
-            className="hidden"
-            ref={fileInput}
-            onChange={handleFileInput}
-          />
+      <div className="w-full">
+        <label htmlFor="mainImg" className="text-neutral-300 inline-block mb-2">
+          <span>Main image</span>
+        </label>
+        <div
+          ref={imageHolder}
+          className="w-full relative py-28 bg-cover bg-center bg-no-repeat rounded-xl overflow-hidden bg-cover bg-no-repeat bg-center bg-blend-overlay"
+        >
+          <div className="absolute inset-0 w-full h-full bg-neutral-600/30 grid place-content-center place-items-center gap-2">
+            <button
+              onClick={triggerFileUpload}
+              className="w-12 h-12 group hover:bg-neutral-100/60 rounded-full bg-neutral-500/50 inline-flex items-center justify-center transition-all duration-300"
+            >
+              <ArrowUp classNames="fill-[#b4ff5c] w-7 transition-all duration-300 group-hover:-translate-y-1 group-hover:fill-neutral-700" />
+            </button>
+            <span className="lg:text-base text-sm text-neutral-200">
+              Add image
+            </span>
+            <input
+              type="file"
+              accept=".jpg, .jpeg, .png, .svg"
+              className="hidden"
+              ref={mainImageInput}
+              onChange={handleFileInput}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="w-full">
+        <label htmlFor="mainImg" className="text-neutral-300 inline-block mb-2">
+          <span>Logo</span>
+        </label>
+        <div
+          ref={logoHolder}
+          className="w-full relative py-20 bg-cover bg-center bg-no-repeat rounded-xl overflow-hidden bg-cover bg-no-repeat bg-center bg-blend-overlay"
+        >
+          <div className="absolute inset-0 w-full h-full bg-neutral-600/30 grid place-content-center place-items-center gap-2">
+            <button
+              onClick={triggerLogoUpload}
+              className="w-12 h-12 group hover:bg-neutral-100/60 rounded-full bg-neutral-500/50 inline-flex items-center justify-center transition-all duration-300"
+            >
+              <ArrowUp classNames="fill-[#b4ff5c] w-7 transition-all duration-300 group-hover:-translate-y-1 group-hover:fill-neutral-700" />
+            </button>
+            <span className="lg:text-base text-sm text-neutral-200">
+              Add image
+            </span>
+            <input
+              type="file"
+              accept=".jpg, .jpeg, .png, .svg"
+              className="hidden"
+              ref={logoInput}
+              onChange={handleLogoInput}
+            />
+          </div>
         </div>
       </div>
       <div className="w-full">
